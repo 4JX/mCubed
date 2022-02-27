6006029d04627f9570bc5f0e3d1c43bba6dcf685
4JX <79868816+4JX@users.noreply.github.com>
2022-02-27 19:44:03 +0100

    Make the storing code generic

--- back/src/persistence.rs
+++ back/src/persistence.rs
@@ -9,45 +9,79 @@ use crate::{
     mod_entry::ModEntry,
 };
 
+use serde::{de::DeserializeOwned, Serialize};
 use tracing::error;
 
 const APP_CACHE_FILE_NAME: &str = "mods.mCubed.json";
 
 pub struct CacheStorage {
-    json_filepath: PathBuf,
-    cache: Vec<ModEntry>,
+    inner: Storage<Vec<ModEntry>>,
 }
 
 impl CacheStorage {
     pub fn new(folder_path: &Path) -> Self {
         let json_filepath: PathBuf = folder_path.join(APP_CACHE_FILE_NAME);
+
         Self {
-            json_filepath,
-            cache: Vec::new(),
+            inner: Storage {
+                json_filepath,
+                storage: Default::default(),
+            },
         }
     }
 
     pub fn load_list_cache(&mut self) -> LibResult<()> {
+        match self.inner.load() {
+            Ok(()) => Ok(()),
+            Err(error) => match error {
+                error::Error::SerdeError(err) => {
+                    error!("Failed to parse cache: {}", err);
+                    Err(error::Error::FailedToParseEntryCache { err })
+                }
+                _ => Err(error),
+            },
+        }
+    }
+
+    pub fn save_list_cache(&self) -> LibResult<()> {
+        self.inner.save()
+    }
+
+    pub fn set_cache(&mut self, new_list: Vec<ModEntry>) {
+        self.inner.set(new_list);
+    }
+
+    pub fn get_cache(&self) -> &Vec<ModEntry> {
+        self.inner.get()
+    }
+
+    pub fn get_cache_mut(&mut self) -> &mut Vec<ModEntry> {
+        self.inner.get_mut()
+    }
+}
+
+pub struct Storage<T> {
+    json_filepath: PathBuf,
+    storage: T,
+}
+
+impl<T> Storage<T>
+where
+    T: DeserializeOwned + Serialize + Default,
+{
+    pub fn load(&mut self) -> LibResult<()> {
         match std::fs::File::open(&self.json_filepath) {
             Ok(file) => {
                 let reader = std::io::BufReader::new(file);
-                match serde_json::de::from_reader(reader) {
-                    Ok(value) => {
-                        self.cache = value;
-                        Ok(())
-                    }
-                    Err(err) => {
-                        error!("Failed to parse cache: {}", err);
-                        Err(error::Error::FailedToParseEntryCache { err })
-                    }
-                }
+                self.storage = serde_json::de::from_reader(reader)?;
+                Ok(())
             }
             Err(err) => match err.kind() {
                 std::io::ErrorKind::NotFound => {
-                    let new_vec = Vec::new();
+                    let new_value = T::default();
                     let mut file = File::create(&self.json_filepath)?;
-                    file.write_all(serde_json::to_string(&new_vec)?.as_bytes())?;
-                    self.cache = new_vec;
+                    file.write_all(serde_json::to_string(&new_value)?.as_bytes())?;
+                    self.storage = new_value;
                     Ok(())
                 }
 
@@ -56,21 +90,21 @@ impl CacheStorage {
         }
     }
 
-    pub fn save_list_cache(&self) -> LibResult<()> {
+    pub fn save(&self) -> LibResult<()> {
         let mut file = File::create(&self.json_filepath)?;
-        file.write_all(serde_json::to_string(&self.cache)?.as_bytes())?;
+        file.write_all(serde_json::to_string(&self.storage)?.as_bytes())?;
         Ok(())
     }
 
-    pub fn set_cache(&mut self, new_list: Vec<ModEntry>) {
-        self.cache = new_list;
+    pub fn set(&mut self, new_storage: T) {
+        self.storage = new_storage;
     }
 
-    pub fn get_cache(&self) -> &Vec<ModEntry> {
-        &self.cache
+    pub fn get(&self) -> &T {
+        &self.storage
     }
 
-    pub fn get_cache_mut(&mut self) -> &mut Vec<ModEntry> {
-        &mut self.cache
+    pub fn get_mut(&mut self) -> &mut T {
+        &mut self.storage
     }
 }
